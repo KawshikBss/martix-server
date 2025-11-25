5bb02d61968f6a73201d43142e9d9be9d7f17a04
KawshikBss <kawshikbss@gmail.com>
2025-11-26 00:34:13 +0600

    added roles routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
     return ['message' => 'API route working'];
 });
 
-require __DIR__.'/../routes/auth.php';
+require __DIR__ . '/../routes/auth.php';
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::group(['prefix' => 'roles'], function () {
+        Route::get('/', [RoleController::class, 'index']);
+        Route::get('/{id}', [RoleController::class, 'show']);
+        Route::post('/', [RoleController::class, 'store']);
+        Route::post('/update/{role}', [RoleController::class, 'update']);
+        Route::post('/assign', [RoleController::class, 'assign']);
+    });
+});
