4b2c492f5d32a4cc7fb70f7c7948feb9fa9190b1
KawshikBss <kawshikbss@gmail.com>
2025-12-15 01:11:35 +0600

    created store resource and collection

---
+++ app/Http/Resources/Store/StoreResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Store;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoreResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'unique_id' => $this->unique_id,
+            'branch' => $this->branch,
+            'type' => $this->type,
+            'description' => $this->description,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'address_2' => $this->address_2,
+            'owner' => $this->owner,
+            'manager' => $this->manager,
+            'is_active' => $this->is_active,
+            'created_at' => $this->created_at->diffForHumans(),
+            'updated_at' => $this->updated_at->diffForHumans()
+        ];
+    }
+}
