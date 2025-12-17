53a09ea5c01688aad652d7638cdb52c46d296567
KawshikBss <kawshikbss@gmail.com>
2025-12-17 12:19:23 +0600

    fixed variations in store

--- app/Http/Controllers/Store/ProductController.php
+++ app/Http/Controllers/Store/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Store;
 use App\Http\Controllers\Controller;
 use App\Models\Store;
 use App\Models\Store\Product;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -17,12 +18,11 @@ class ProductController extends Controller
         $managerStoreIds = Store::where('manager_id', $user->id)->pluck('id')->toArray();
         $storeIds = array_merge($storeIds, $managerStoreIds);
         $productsInStores = DB::table('store_products')->whereIn('store_id', $storeIds)->pluck('product_id')->toArray();
-        // return dd($productsInStores);
         $products = Product::where(function ($q) use ($productsInStores) {
             foreach ($productsInStores as $productId) {
                 $q->where('id', $productId);
             }
-        })->orWhere('owner_id', $user->id)->with('category')->get();
+        })->orWhere('owner_id', $user->id)->where('is_variation', false)->with(['category', 'variants'])->get();
         return response()->json($products);
     }
 
@@ -43,14 +43,19 @@ class ProductController extends Controller
             'description' => 'nullable|string',
             'image' => 'nullable|image|max:2048',
             'cost_price' => 'required|numeric|min:0',
+            'tax_type' => 'nullable|string|max:100',
+            'tax_rate' => 'nullable|numeric|min:0',
             'category_id' => 'nullable|exists:categories,id',
             'brand' => 'nullable|string|max:255',
             'tags' => 'nullable|string',
-            'parent_id' => 'nullable|exists:products,id',
+            // 'parent_id' => 'nullable|exists:products,id',
             'is_variation' => 'sometimes|boolean',
-            'variations' => 'nullable|json',
             'is_active' => 'sometimes|boolean',
+            'variations' => 'nullable|array'
         ]);
+
+        $variations = $data['variations'];
+        unset($data['variations']);
 
         if ($request->hasFile('image')) {
             $imagePath = $request->file('image')->store('products', 'public');
@@ -61,9 +66,34 @@ class ProductController extends Controller
         $data['image'] = $imagePath;
 
         $data['owner_id'] = $user->id;
+        DB::beginTransaction();
 
-        $product = $user->products()->create($data);
-        return response()->json($product, 201);
+
+        try {
+            $product = $user->products()->create($data);
+            if (count($variations) > 0) {
+                unset($data['sku']);
+                $data['is_variation'] = true;
+                foreach ($variations as $variation) {
+                    $newProduct = $product->variants()->create($data);
+                    $productOption = $newProduct->options()->create([
+                        'name' => $variation['option']
+                    ]);
+                    foreach ($variation['values'] as $value) {
+                        $productOption->values()->create([
+                            'value' => $value
+                        ]);
+                    }
+                }
+            }
+
+            DB::commit();
+
+            return response()->json($product, 201);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     public function update(Request $request, $id)
@@ -77,12 +107,13 @@ class ProductController extends Controller
             'description' => 'nullable|string',
             'image' => 'nullable|image|max:2048',
             'cost_price' => 'sometimes|required|numeric|min:0',
+            'tax_type' => 'nullable|string|max:100',
+            'tax_rate' => 'nullable|numeric|min:0',
             'category_id' => 'nullable|exists:categories,id',
             'brand' => 'nullable|string|max:255',
             'tags' => 'nullable|string',
             'parent_id' => 'nullable|exists:products,id',
             'is_variation' => 'sometimes|boolean',
-            'variations' => 'nullable|json',
             'is_active' => 'sometimes|boolean',
         ]);
 
