805043e9c9efb7c055c28f841a74426e1096ed56
KawshikBss <kawshikbss@gmail.com>
2025-12-17 12:17:50 +0600

    added variation attributes and relations

--- app/Models/Store/Product.php
+++ app/Models/Store/Product.php
@@ -3,12 +3,20 @@
 namespace App\Models\Store;
 
 use App\Models\Category;
+use App\Models\Store\Product\ProductOption;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     protected $guarded = [];
+
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute(): string
+    {
+        return  env('APP_URL') . '/storage/' . ($this->image ? $this->image : 'products/default-user.png');
+    }
 
     public function owner()
     {
@@ -18,5 +26,20 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Product::class, 'parent_id');
+    }
+
+    public function variants()
+    {
+        return $this->hasMany(Product::class, 'parent_id')->with(['category', 'options']);
+    }
+
+    public function options()
+    {
+        return $this->hasMany(ProductOption::class)->with('values');
     }
 }
