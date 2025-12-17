ab8d4686f3e44f4a8c8af761969f64d6f5fbe61b
KawshikBss <kawshikbss@gmail.com>
2025-12-17 12:17:14 +0600

    added tax columns to products table and removed variations column

---
+++ database/migrations/2025_12_16_122209_add_columns_to_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('tax_type')->nullable()->after('cost_price');
+            $table->decimal('tax_rate', 10, 2)->nullable()->after('tax_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('variations');
+        });
+    }
+};
