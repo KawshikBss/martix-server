9114bf491ed51d85ee85d14241f74f271b0fef6f
KawshikBss <kawshikbss@gmail.com>
2025-12-19 20:53:56 +0600

    added scope by owner

--- app/Models/Store/Inventory/Inventory.php
+++ app/Models/Store/Inventory/Inventory.php
@@ -4,6 +4,7 @@ namespace App\Models\Store\Inventory;
 
 use App\Models\Store;
 use App\Models\Store\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
@@ -23,5 +24,21 @@ class Inventory extends Model
     public function movements()
     {
         return $this->hasMany(InventoryMovement::class);
+    }
+
+    public function scopeOwnedByUser($query, User $user)
+    {
+        return $query->where(function ($q) use ($user) {
+            $q->whereHas(
+                'store',
+                fn($s) =>
+                $s->where('owner_id', $user->id)
+            )
+                ->orWhereHas(
+                    'product',
+                    fn($p) =>
+                    $p->where('owner_id', $user->id)
+                );
+        });
     }
 }
