a9f5366c1537db00d12d5717e66dd39e0c1a2ae9
KawshikBss <kawshikbss@gmail.com>
2025-12-29 19:09:07 +0600

    fixed filters

--- app/Http/Controllers/Store/StoreController.php
+++ app/Http/Controllers/Store/StoreController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Store;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Store\StoreCollection;
 use App\Http\Resources\Store\StoreResource;
 use App\Models\Store;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class StoreController extends Controller
@@ -14,14 +16,114 @@ class StoreController extends Controller
     public function index(Request $request)
     {
         $user = auth()->user();
+        $stores = $user->stores();
         $search = $request->query('query', null);
-        $stores = $user->stores;
         if ($search != null && $search !== '') {
             $like = "%{$search}%";
-            $stores = $user->stores()->where('name', 'like', $like)->orWhere('unique_id', $like)->get();
+            $stores = $stores->where('name', 'like', $like)->orWhere('unique_id', $like);
         }
-        $stores = StoreResource::collection($stores);
-        return response()->json($stores);
+
+        $manager = $request->query('manager', null);
+        if ($manager != null && $manager !== '') {
+            $stores = $stores->where('manager_id', $manager);
+        }
+
+        $branch = $request->query('branch', null);
+        if ($branch != null && $branch !== '') {
+            $like = "%{$branch}%";
+            $stores = $stores->where('branch', 'like', $like);
+        }
+
+        $location = $request->query('location', null);
+        if ($location != null && $location !== '') {
+            $like = "%{$location}%";
+            $stores = $stores->where('address', 'like', $like)->orWhere('address_2', 'like', $like);
+        }
+
+        $status = $request->query('status', null);
+        if ($status != null && $status !== '') {
+            $stores = $stores->where('is_active', $status === 'active' ? true : false);
+        }
+
+        $type = $request->query('type', null);
+        if ($type != null && $type !== '') {
+            $like = "%{$type}%";
+            $stores = $stores->where('type', 'like', $like);
+        }
+
+        $minInventoryValue = $request->query('min_inventory_value', null);
+        $maxInventoryValue = $request->query('max_inventory_value', null);
+
+        if ($minInventoryValue !== null || $maxInventoryValue !== null) {
+
+            $stores = $stores->withSum([
+                'inventories as inventory_value' => function ($q) {
+                    $q->select(DB::raw('SUM(quantity * selling_price)'));
+                }
+            ], 'quantity');
+
+            if ($minInventoryValue !== null && $minInventoryValue !== '') {
+                $stores->having('inventory_value', '>=', $minInventoryValue);
+            }
+
+            if ($maxInventoryValue !== null && $maxInventoryValue !== '') {
+                $stores->having('inventory_value', '<=', $maxInventoryValue);
+            }
+        }
+
+        $hasStaff = $request->query('has_staff', null);
+        if ($hasStaff != null && $hasStaff === 'true') {
+            $stores = $stores->whereHas('staff');
+        }
+
+        $hasLowStock = $request->query('has_low_stock', null);
+        if ($hasLowStock != null && $hasLowStock === 'true') {
+            $stores = $stores->whereHas('inventories', function ($q) {
+                $q->whereColumn('quantity', '<=', 'reorder_level');
+            });
+        }
+
+        $hasExpiredProducts = $request->query('has_expired_products', null);
+        if ($hasExpiredProducts != null && $hasExpiredProducts === 'true') {
+            $stores = $stores->whereHas('inventories', function ($q) {
+                $q->where('expiry_date', '<', Carbon::now());
+            });
+        }
+
+        $hasProductsExpiringSoon = $request->query('has_soon_expiring_products', null);
+        if ($hasProductsExpiringSoon != null && $hasProductsExpiringSoon === 'true') {
+            $stores = $stores->whereHas('inventories', function ($q) {
+                $q->where('expiry_date', '<', Carbon::now()->addDays(10));
+            });
+        }
+
+        $minCreateDate = $request->query('min_create_date', null);
+        if ($minCreateDate != null && $minCreateDate !== '') {
+            $date = Carbon::parse($minCreateDate);
+            $stores = $stores->where('created_at', '>=', $date);
+        }
+
+        $maxCreateDate = $request->query('max_create_date', null);
+        if ($maxCreateDate != null && $maxCreateDate !== '') {
+            $date = Carbon::parse($maxCreateDate);
+            $stores = $stores->where('created_at', '<=', $date);
+        }
+
+        $minUpdateDate = $request->query('min_update_date', null);
+        if ($minUpdateDate != null && $minUpdateDate !== '') {
+            $date = Carbon::parse($minUpdateDate);
+            $stores = $stores->where('updated_at', '>=', $date);
+        }
+
+        $maxUpdateDate = $request->query('max_update_date', null);
+        if ($maxUpdateDate != null && $maxUpdateDate !== '') {
+            $date = Carbon::parse($maxUpdateDate);
+            $stores = $stores->where('updated_at', '<=', $date);
+        }
+
+        $stores = $stores->paginate(2);
+
+        return new StoreCollection($stores);
     }
 
     public function show($id)
