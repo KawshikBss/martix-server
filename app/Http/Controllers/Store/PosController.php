1fcfadd333809fedcb35891fa52103cbe3518ee4
KawshikBss <kawshikbss@gmail.com>
2026-01-09 16:17:32 +0600

    added create order function

--- app/Http/Controllers/Store/PosController.php
+++ app/Http/Controllers/Store/PosController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Store;
 use App\Http\Controllers\Controller;
 use App\Models\Store\Inventory\Inventory;
 use App\Models\Store\Product;
+use App\Models\Store\Sale\Sale;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PosController extends Controller
 {
@@ -82,10 +85,69 @@ class PosController extends Controller
         ]);
     }
 
-    public function getCustomers()
+    public function createSale(Request $request)
     {
-        $authUser = auth()->user();
-        $users = User::whereNot('id', $authUser->id)->where('status', 'active')->get();
-        return response()->json($users);
+
+        $user = auth()->user();
+
+        $data = $request->validate([
+            'store_id' => 'required',
+            'customer_id' => 'nullable',
+            'sub_total' => 'required|numeric|min:0',
+            'tax_total' => 'required|numeric|min:0',
+            'discount_total' => 'required|numeric|min:0',
+            'grand_total' => 'required|numeric|min:0',
+            'paid_amount' => 'required|numeric|min:0',
+            'due_amount' => 'required|numeric|min:0',
+            'payment_method' => 'nullable|string|max:255',
+            'payment_details' => 'nullable|array',
+            'items' => 'array'
+        ]);
+
+        $saleItems = $data['items'];
+        unset($data['items']);
+        $data['user_id'] = $user->id;
+
+        $saleNumber = 'POS-' . strtoupper(uniqid());
+        $data['sale_number'] = $saleNumber;
+
+        if ($data['due_amount'] > 0) {
+            if ($data['paid_amount'] == 0) {
+                $data['payment_status'] = 'unpaid';
+            } else {
+                $data['payment_status'] = 'partial';
+            }
+        } else {
+            $data['payment_status'] = 'paid';
+        }
+
+        if ($data['payment_details'] && count($data['payment_details'])) {
+            $data['payment_details'] = json_encode($data['payment_details']);
+        }
+
+        DB::beginTransaction();
+
+        try {
+
+            $sale = Sale::create($data);
+
+            foreach ($saleItems as $item) {
+                $sale->items()->create([
+                    'product_id' => $item['product_id'],
+                    'inventory_id' => $item['inventory_id'],
+                    'quantity' => $item['quantity'],
+                    'price' => $item['price'],
+                    'tax' => $item['tax'],
+                    'discount' => 0,
+                    'tax' => $item['tax'],
+                    'total' => $item['itemTotal'],
+                ]);
+            }
+            DB::commit();
+            return response()->json($sale, 201);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
