d405d80ab4e2b4641da7aa73e31563639f8df540
KawshikBss <kawshikbss@gmail.com>
2026-02-18 19:18:02 +0600

    created table for inventory transfers

---
+++ database/migrations/2026_02_18_125426_create_inventory_transfers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_transfers', function (Blueprint $table) {
+            $table->id();
+            $table->string('transfer_number')->unique();
+            $table->unsignedBigInteger('source_store_id');
+            $table->foreign('source_store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->unsignedBigInteger('destination_store_id');
+            $table->foreign('destination_store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->string('status')->nullable();
+            $table->unsignedBigInteger('created_by_id');
+            $table->foreign('created_by_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_transfers');
+    }
+};
