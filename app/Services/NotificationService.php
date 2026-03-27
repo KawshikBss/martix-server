d29f441ac5cd48c2d17ae5444303dd11c0e4a72d
KawshikBss <kawshikbss@gmail.com>
2026-03-27 15:16:21 +0600

    created notification service

---
+++ app/Services/NotificationService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Notifications\InventoryNotification;
+use Illuminate\Support\Facades\Auth;
+
+class NotificationService
+{
+    public function notifyInventoryStatus($data, $audience = [])
+    {
+        if (count($audience) === 0) {
+            $audience[] = Auth::user();
+        }
+
+        $notificationData = [];
+        $productName = $data['inventory']['product']['name'];
+        $notificationData['action_url'] = '/dashboard/inventory/' . $data['inventory']['id'];
+        $notificationData['reference_type'] = "inventory";
+        $notificationData['reference_id'] = $data['inventory']['id'];
+
+        switch ($data['type']) {
+            case 'low_stock':
+                $notificationData['title'] = 'Low Stock';
+                $notificationData['messge'] = $productName . ' is running low.';
+                $notificationData['priority'] = 'high';
+                break;
+            case 'out_of_stock':
+                $notificationData['title'] = 'Out of Stock';
+                $notificationData['messge'] = $productName . ' is out of stock.';
+                $notificationData['priority'] = 'high';
+                break;
+        }
+        foreach ($audience as $recipient) {
+            $recipient->notify(new InventoryNotification($notificationData));
+        }
+    }
+}
